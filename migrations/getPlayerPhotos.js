eb639fa5f36622b4b9b8b65cf64ae5fae0000dbf
Matt Sell <mwsell@gmail.com>
2015-08-31 21:27:42 -0500

    screen scrape photo

--- migrations/getPlayerPhotos.js
+++ migrations/getPlayerPhotos.js
@@ -2,11 +2,13 @@
 var MongoClient = require('mongodb').MongoClient;
 var cheerio = require('cheerio');
 var request = require('request');
+var superagent = require('superagent');
+var config = require('./config.json');
 
 // Connect to the db
 MongoClient.connect(process.env.GRIDIRON_DB, function (err, db) {
   if (!err) {
-    console.log("We are connected");
+
     db.open(function (err, db) {
       db.collection('player', function (err, collection) {
         collection.find(function (err, cursor) {
@@ -14,17 +16,26 @@ MongoClient.connect(process.env.GRIDIRON_DB, function (err, db) {
             if (player != null && player.nflId) {
 
               var url = 'http://www.nfl.com/player/' + player.nflId + '/profile';
-              console.log(url);
+              //console.log(url);
               request(url, function(err, res, html){
                 if(!err && res.statusCode === 200){
                   var $ = cheerio.load(html);
-                  console.log(html);
-                  var selector = $('.player-photo').get();
-                  console.log(selector);
+                  var img = $('.player-photo img').attr('src');
+
+                  console.log(config.url);
+                  superagent.put(config.url + 'player/' + player._id)
+                  .send({
+                    photo: img
+                  })
+                  .on('response', function (updateRes) {
+
+                    console.log(updateRes.statusCode + ' ' + config.url + 'player/' + player._id);
+                  })
+                  .end();
                 }
               })
-              
-              
+
+
             } else {
               db.close();
             }
