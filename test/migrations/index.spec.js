75299f0b70ac732186b266e294de38ef08bc904a
Matt Sell <mwsell@gmail.com>
2015-09-11 21:35:19 -0500

    add typings

---
+++ test/migrations/index.spec.js
@@ -0,0 +1,11 @@
+var migrate = require('../../migrations'),
+    request = require('supertest'),
+    expect = require('expect'),
+    chai = require('chai');
+
+
+describe('DB Migrations', function () {
+    describe('when database already exists', function () {
+        it('should not import teams')
+    });
+});
